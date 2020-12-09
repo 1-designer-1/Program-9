d47a0422a90eea4e85e2d0f036d6d3c8015584b7
исп-31 <-31@LAB17-13>
2020-12-09 15:08:09 +0300

    1

--- Program 9/Form1.cs
+++ Program 9/Form1.cs
@@ -11,8 +11,8 @@ using System.Windows.Forms;
 namespace Program_9
 {
     public partial class Form1 : Form
-    {   
-        Bagage [] bagages;
+    {
+        Bagage[] bagages;
         public Form1()
         {
             InitializeComponent();
@@ -21,13 +21,13 @@ namespace Program_9
             dataGridView1.RowCount = 10;
         }
 
-        
+
         private void button1_Click(object sender, EventArgs e)
         {
-            int pos = (int)numericUpDown1.Value -1;
+            int pos = (int)numericUpDown1.Value - 1;
             bagages[pos].CountItem = Convert.ToInt32(textBox1.Text);
-            bagages [pos].Weight = Convert.ToInt32(textBox2.Text);
-            dataGridView1[0,pos].Value = pos+1;
+            bagages[pos].Weight = Convert.ToInt32(textBox2.Text);
+            dataGridView1[0, pos].Value = pos + 1;
             dataGridView1[1, pos].Value = textBox1.Text;
             dataGridView1[2, pos].Value = textBox2.Text;
         }
