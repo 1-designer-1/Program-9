401b1dd0f888e854fd16eccf82ef03c11e6b1df9
Группа ИСП-31 <-31@rssk.local>
2020-12-09 14:13:19 +0300

    Добавьте файлы проекта.

---
+++ Program 9/Bagage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_9
+{
+    struct Bagage
+    {
+        public int CountItem;
+        public decimal Weight;
+
+        public Bagage (int countItem, decimal weight)
+        {
+            CountItem = countItem;
+            Weight = weight;
+        }
+
+
+
+    }
+}
